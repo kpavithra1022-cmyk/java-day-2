4381f8c97ebeb5b70b780f1dd083f685416825d1
kpavithra1022-cmyk <kpavithra1022@gmail.com>
2026-02-07 11:05:38 +0530

    Add files via upload

---
+++ primitive.java
@@ -0,0 +1,12 @@
+public class primitive{
+    public static void main(String[] args) {
+        int hin=85;
+        int eng=95;
+        int mat=98;
+        int sum=(hin+eng+mat);
+        int avg=sum/3;
+        System.out.println(sum);
+        System.out.println(avg);
+
+    } 
+}
